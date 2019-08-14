ca0eb4fe4954fdd065ef943946a5bb04a38bc6f9
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-08-14 04:03:33 +0000

    Enable language to be set just once in translate service

--- src/Model/Service/Translate.php
+++ src/Model/Service/Translate.php
@@ -5,16 +5,33 @@ use LeoGalleguillos\Translate\Model\Service as TranslateService;
 
 class Translate
 {
+    protected $language;
+
     public function __construct(
         TranslateService\GetArray $getArrayService
     ) {
         $this->getArrayService = $getArrayService;
     }
 
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(string $language): TranslateService\Translate
+    {
+        $this->language = $language;
+        return $this;
+    }
+
     public function translate(
         string $string,
-        string $language
-    ) : string {
+        string $language = null
+    ): string {
+        if (!isset($language)) {
+            $language = $this->getLanguage();
+        }
+
         $array = $this->getArrayService->getArray($language);
         return $array[$string] ?? '';
     }
