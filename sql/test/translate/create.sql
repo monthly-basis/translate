fb0dfdbde95a546cc5c5978e17f8dc3308896eb6
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-03-25 06:19:07 +0000

    Update case

--- sql/test/translate/create.sql
+++ sql/test/translate/create.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `translate` (
-    `translate_id` int(10) unsigned not null auto_increment,
-    `en` varchar(255) not null,
-    `es` varchar(255) default null,
-    `fr` varchar(255) default null,
+    `translate_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `en` varchar(255) NOT NULL,
+    `es` varchar(255) DEFAULT NULL,
+    `fr` varchar(255) DEFAULT NULL,
+    `st` varchar(255) DEFAULT NULL,
+    `sw` varchar(255) DEFAULT NULL,
+    `tl` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`translate_id`),
     UNIQUE `en` (`en`)
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
